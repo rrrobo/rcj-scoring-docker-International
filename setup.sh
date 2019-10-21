0cf61468eb7804f1a9547532e63483255888ef37
rrrobo <rrrobo@icloud.com>
2019-10-21 12:52:53 +0100

    add fonts directory

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ echo "Create the necessary directories"
 mkdir -p logs
 mkdir -p db
 mkdir -p TechnicalDocument
+mkdir -p fonts
 echo "Download the latest version of the image from Docker Cloud"
 sudo docker pull ryorobo/rcj-rescue-scoring-japan:latest
 echo "Launch the image"
