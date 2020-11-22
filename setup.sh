83d7729fe199aa67e731e1d73192807844598872
rrrobo <rrrobo@icloud.com>
2020-11-23 02:39:33 +0900

    update setup script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 echo "Create the necessary directories"
 mkdir -p logs
 mkdir -p db
-mkdir -p TechnicalDocument
+mkdir -p documents
 mkdir -p fonts
 echo "Download the latest version of the image from Docker Cloud"
 sudo docker pull ryorobo/rcj-rescue-scoring-japan:latest
