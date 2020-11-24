0e13b62170f49e3093aeca5e4e37b44b19503977
rrrobo <rrrobo@icloud.com>
2020-11-24 19:49:44 +0900

    mailTemplates

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ mkdir -p logs
 mkdir -p db
 mkdir -p documents
 mkdir -p fonts
+mkdir -p mailTemplates
 echo "Download the latest version of the image from Docker Cloud"
 sudo docker pull ryorobo/rcj-rescue-scoring-japan:latest
 echo "Launch the image"
